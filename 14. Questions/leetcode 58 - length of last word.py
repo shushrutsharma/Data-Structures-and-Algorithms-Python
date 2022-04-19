4882a16a2b5417f12343ad461336c4354dde0125
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2022-04-19 19:45:06 +0530

    length of last word without inbuilt funtions added

--- 14. Questions/leetcode 58 - length of last word.py
+++ 14. Questions/leetcode 58 - length of last word.py
@@ -7,9 +7,23 @@ Question: https://leetcode.com/problems/length-of-last-word/
 
 """
 
+#Solution usinf inbuilt fuctions : 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()
         s = s.split(" ")
         last = s[len(s)-1]
         return (len(last))
+
+
+#Solution without inbuilt fuctions : 
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i, length = len(s)-1, 0
+        while s[i] == " ":
+            i -= 1
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length
+            
