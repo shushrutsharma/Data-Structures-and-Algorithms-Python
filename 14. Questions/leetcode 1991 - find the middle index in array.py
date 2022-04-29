46108a5bd4fe9ff901386340ab3ff6b2d037c4f0
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2022-04-29 17:27:34 +0530

    finding middle/pivot index added

---
+++ 14. Questions/leetcode 1991 - find the middle index in array.py
@@ -0,0 +1,26 @@
+"""
+
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+Question: https://leetcode.com/problems/find-the-middle-index-in-array/
+
+"""
+
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        
+        leftSum = 0
+        for i in range(len(nums)):
+            rightSum = total - nums[i] - leftSum
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+        return -1
---
+++ 14. Questions/leetcode 724 - find pivot index.py
@@ -0,0 +1,25 @@
+"""
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Question : https://leetcode.com/problems/find-pivot-index/
+
+
+"""
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        
+        leftSum = 0
+        for i in range(len(nums)):
+            rightSum = total - nums[i] - leftSum
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+        return -1
