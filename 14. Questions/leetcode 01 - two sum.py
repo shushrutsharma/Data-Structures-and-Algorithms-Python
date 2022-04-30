aad5a5d4659c725dc573625edc065d86211763d2
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2022-04-30 18:12:25 +0530

    linear time solution for two sum added (Hash Maps)

--- 14. Questions/leetcode 01 - two sum.py
+++ 14. Questions/leetcode 01 - two sum.py
@@ -8,7 +8,7 @@ You can return the answer in any order.
 Question: https://leetcode.com/problems/two-sum/
 
 """
-
+#O(n)^2 solution - brute force
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -16,3 +16,16 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return []
+
+#O(n) solution - hash maps
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target-n
+            if diff in prevMap:
+                return[prevMap[diff],i]
+            prevMap[n] = i
+        return
