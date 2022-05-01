d1bfc8baa01d63033b69616ce3ca9ab983e24c6d
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2022-05-01 22:27:52 +0530

    alternate solution for intersecting arrays added

--- 14. Questions/leetcode 350 - intersection of two arrays II.py
+++ 14. Questions/leetcode 350 - intersection of two arrays II.py
@@ -6,6 +6,8 @@ Question: https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 
 """
+
+#solution 1
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         c = Counter(nums1)
@@ -16,3 +18,25 @@ class Solution:
                 c[n]-=1
 
         return (output)
+
+#solution 2
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i=0
+        j=0
+        output = []
+        nums1.sort()
+        nums2.sort()
+
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                i = i+1
+            elif nums1[i] > nums2[j]:
+                j = j+1
+            else:
+                output.append(nums1[i])
+                i=i+1
+                j=j+1
+
+        return output
