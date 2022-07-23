1405ca6e9b3c20a32d781325ae3da8616ba83614
mihirs16 <mihirs16@gmail.com>
2022-07-23 15:43:14 +0530

    added lowest common ancestor

---
+++ 14. Questions/leetcode 235 - lowest common ancestor in bst.py
@@ -0,0 +1,23 @@
+# lowest common ancestor in binary search tree | leetcode 235 | https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# method: iteration through each node, when p and q are in different subtrees, current node is LCA
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+        
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
+
+        return root
